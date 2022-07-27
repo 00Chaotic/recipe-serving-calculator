d0adff528ae3b519a216504f582db40bc4b9599f
Jaffar Rizvi <J.Rizvi@outlook.com>
2022-07-27 20:41:52 +1000

    Convert components to their own classes

--- src/calculator/Calculator.js
+++ src/calculator/Calculator.js
@@ -1,34 +1,44 @@
 import { Fab, List, Stack, TextField } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
-import { useState } from "react";
+import React from "react";
 import IngredientListItem from "../components/IngredientListItem/IngredientListItem";
 
-function Calculator() {
-    const [listItems, setListItems] = useState([]);
+class Calculator extends React.Component {
 
-    return (
-        <>
-            <TextField label="Recipe Name" variant="standard" autoComplete="off" />
-            <List>
-                {listItems}
-            </List>
-            <Stack direction="row" spacing={2}>
-                <TextField label="Current Servings" variant="outlined" autoComplete="off" type={"number"} />
-                <TextField label="Desired Servings" variant="outlined" autoComplete="off" type={"number"} />
-            </Stack>
-            <Fab color="secondary" size="medium" onClick={() => addListitemHandler(setListItems)}>
-                <AddIcon/>
-            </Fab>
-        </>
-    );
+    constructor(props) {
+        super(props);
+        this.state = { listItems: [], listItemKeyCounter: 0 };
+    }
+
+    addListitemHandler = () => {
+        return this.setState((state, props) => ({
+            listItems: [...state.listItems, <IngredientListItem deleteHandler={this.removeListItemHandler} key={state.listItemKeyCounter + 1}/>],
+            listItemKeyCounter: state.listItemKeyCounter + 1
+        }));
+    }
+    
+    removeListItemHandler = () => {
+        return this.setState((state, props) => ({
+            listItems: state.listItems.pop()
+        }));
+    }
+
+    render() {
+        return (
+            <>
+                <TextField label="Recipe Name" variant="standard" autoComplete="off" />
+                <List>
+                    {this.state.listItems}
+                </List>
+                <Stack direction="row" spacing={2}>
+                    <TextField label="Current Servings" variant="outlined" autoComplete="off" type={"number"} />
+                    <TextField label="Desired Servings" variant="outlined" autoComplete="off" type={"number"} />
+                </Stack>
+                <Fab color="secondary" size="medium" onClick={this.addListitemHandler}>
+                    <AddIcon/>
+                </Fab>
+            </>
+        );
+    } 
 }
-
-const addListitemHandler = (setListItems) => {
-    return setListItems(current => [...current, <IngredientListItem id={current.length + 1} deleteHandler={() => removeListItemHandler(setListItems)} key={current.length + 1}/>]);
-}
-
-const removeListItemHandler = (setListItems) => {
-    return setListItems(current => current.filter((_, index) => index !== current.length));
-}
-
 export default Calculator;
--- src/components/IngredientListItem/IngredientListItem.js
+++ src/components/IngredientListItem/IngredientListItem.js
@@ -1,16 +1,22 @@
 import { Stack, ListItem, TextField, Button } from "@mui/material";
 import DeleteIcon from '@mui/icons-material/Delete';
+import React from "react";
 
-function IngredientListItem(props) {
-    return <ListItem disableGutters>
-        <Stack direction="row" spacing={2}>
-            <TextField label="Item Name" variant="outlined" fullWidth autoComplete="off" />
-            <TextField label="Quantity" variant="outlined" autoComplete="off" type={"number"} />
-            <Button onClick={props.deleteHandler}>
-                <DeleteIcon color="action" fontSize="large"/>
-            </Button>
-        </Stack>
-    </ListItem>
+class IngredientListItem extends React.Component {
+    
+    render() {
+        return <>
+            <ListItem disableGutters>
+                <Stack direction="row" spacing={2}>
+                    <TextField label="Item Name" variant="outlined" fullWidth autoComplete="off" />
+                    <TextField label="Quantity" variant="outlined" autoComplete="off" type={"number"} />
+                    <Button onClick={this.props.deleteHandler}>
+                        <DeleteIcon color="action" fontSize="large"/>
+                    </Button>
+                </Stack>
+            </ListItem>
+        </>
+    }
 }
 
 export default IngredientListItem;
