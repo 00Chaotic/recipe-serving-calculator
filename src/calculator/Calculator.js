59f03fa3e5ed2c9d8f566840a8ec9b562b5b1a78
Jaffar Rizvi <J.Rizvi@outlook.com>
2022-07-28 21:15:09 +1000

    Refactoring code and finishing basic functionality

--- src/calculator/Calculator.js
+++ src/calculator/Calculator.js
@@ -7,57 +7,85 @@ import './Calculator.css';
 class Calculator extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {listItems: [
-            <IngredientListItem
-                deleteHandler={this.removeListItemHandler}
-                id={0}
-                key={0}
-            />,
-        ], listItemKeyCounter: 1};
+        this.state = {
+            listItems: [
+                <IngredientListItem
+                    calculatedQuantity={0}
+                    changeHandler={this.handleQuantityChange}
+                    currentQuantity={0}
+                    deleteHandler={this.removeListItemHandler}
+                    id={0}
+                    key={0}
+                />,
+            ],
+            listItemKeyCounter: 1,
+            currentServings: 0,
+            desiredServings: 0,
+        };
     }
 
     addListitemHandler = () => {
-        return this.setState((state, props) => ({
+        this.setState((prevState, props) => ({
             listItems: [
-                ...state.listItems,
+                ...prevState.listItems,
                 <IngredientListItem
+                    calculatedQuantity={0}
+                    changeHandler={this.handleQuantityChange}
+                    currentQuantity={0}
                     deleteHandler={this.removeListItemHandler}
-                    id={state.listItemKeyCounter + 1}
-                    key={state.listItemKeyCounter + 1}
+                    id={prevState.listItemKeyCounter + 1}
+                    key={prevState.listItemKeyCounter + 1}
                 />,
             ],
-            listItemKeyCounter: state.listItemKeyCounter + 1,
+            listItemKeyCounter: prevState.listItemKeyCounter + 1,
         }));
     };
 
     removeListItemHandler = (id) => {
-        return this.setState((state, props) => ({
-            listItems: state.listItems.filter((item) => item.props.id !== id),
+        this.setState((prevState, props) => ({
+            listItems: prevState.listItems.filter((item) => item.props.id !== id),
         }));
+    };
+
+    handleQuantityChange = (listItemIndex, value) => {
+        this.setState((prevState) => ({
+            listItems: prevState.listItems.map((element) => element.props.id == listItemIndex ? {...element, props: {...element.props, currentQuantity: value}} : element),
+        }));
+    };
+
+    calculateQuantities = () => {
+        const newList = [];
+        this.state.listItems.forEach((element, index) => {
+            const calculatedQuantity = element.props.currentQuantity * (this.state.desiredServings / this.state.currentServings);
+            newList[index] = {
+                ...element,
+                props: {...element.props, calculatedQuantity: calculatedQuantity}};
+        });
+        this.setState({listItems: newList});
     };
 
     render() {
         return (
             <Box>
-                <TextField label="Recipe Name" variant="standard"
-                    autoComplete="off" />
+                <TextField label="Recipe Name" variant="standard" inputProps={{autoComplete: 'off'}} />
                 <List>
                     {this.state.listItems}
                 </List>
                 <Stack direction="row" spacing={2}>
-                    <TextField label="Current Servings" variant="outlined" autoComplete="off"
-                        type={'number'} />
-                    <TextField label="Desired Servings" variant="outlined" autoComplete="off"
-                        type={'number'} />
+                    <TextField label="Current Servings" variant="outlined" defaultValue={0} type={'number'} inputProps={{autoComplete: 'off'}}
+                        onChange={(event) => this.setState({currentServings: parseFloat(event.target.value) || 0})} />
+                    <TextField label="Desired Servings" variant="outlined" defaultValue={0} type={'number'} inputProps={{autoComplete: 'off'}}
+                        onChange={(event) => this.setState({desiredServings: parseFloat(event.target.value) || 0})}/>
                 </Stack>
                 <Stack className='button-stack' direction="row" spacing={2}>
                     <Fab color="secondary" size="medium" onClick={this.addListitemHandler}>
                         <AddIcon/>
                     </Fab>
-                    <Button className='calculate-button' variant='contained'>Calculate</Button>
+                    <Button className='calculate-button' variant='contained' onClick={this.calculateQuantities}>Calculate</Button>
                 </Stack>
             </Box>
         );
     }
 }
+
 export default Calculator;
--- src/components/IngredientListItem/IngredientListItem.js
+++ src/components/IngredientListItem/IngredientListItem.js
@@ -8,9 +8,11 @@ class IngredientListItem extends React.Component {
         return <>
             <ListItem disableGutters>
                 <Stack direction="row" spacing={2}>
-                    <TextField label="Item Name" variant="outlined" fullWidth autoComplete="off" />
-                    <TextField label="Quantity" variant="outlined" autoComplete="off"
-                        type={'number'} />
+                    <TextField label="Item Name" variant="outlined" inputProps={{autoComplete: 'off'}} fullWidth />
+                    <TextField label="Quantity" defaultValue={0} variant="outlined" autoComplete="off" type={'number'}
+                        inputProps={{autoComplete: 'off', min: 0}} onChange={(event) => this.props.changeHandler(this.props.id, (parseFloat(event.target.value)) || 0)} />
+                    <TextField label="Calculation" variant="outlined" value={this.props.calculatedQuantity}
+                        inputProps={{readOnly: true, disabled: true}} />
                     <Button onClick={() => this.props.deleteHandler(this.props.id)}>
                         <DeleteIcon color="action" fontSize="large"/>
                     </Button>
@@ -21,6 +23,8 @@ class IngredientListItem extends React.Component {
 }
 
 IngredientListItem.propTypes = {
+    calculatedQuantity: PropTypes.number.isRequired,
+    changeHandler: PropTypes.func.isRequired,
     deleteHandler: PropTypes.func.isRequired,
     id: PropTypes.number.isRequired,
 };
