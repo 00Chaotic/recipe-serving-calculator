930957bd630d89b4c2fdcaff536babe7a75b9f95
Jaffar Rizvi <J.Rizvi@outlook.com>
2022-08-03 21:44:31 +1000

    Add unit measurements/conversions

--- src/calculator/Calculator.js
+++ src/calculator/Calculator.js
@@ -8,19 +8,34 @@ class Calculator extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            currentServings: 0,
+            desiredServings: 0,
+            listItemKeyCounter: 1,
             listItems: [
                 <IngredientListItem
                     calculatedQuantity={0}
-                    changeHandler={this.handleQuantityChange}
                     currentQuantity={0}
                     deleteHandler={this.removeListItemHandler}
+                    fromUnit={0}
                     id={0}
                     key={0}
+                    toUnit={0}
+                    unitChangeHandler={this.handleUnitChange}
+                    quantityChangeHandler={this.handleQuantityChange}
                 />,
             ],
-            listItemKeyCounter: 1,
-            currentServings: 0,
-            desiredServings: 0,
+            // Conversion matrix for measurement units
+            conversionRatios: [
+                [1, 0.0625, 0.02083, 4.22675, 0.00422, 2.26, 0.125, 0.00423, 4.22675],
+                [16, 1, 0.33333, 128, 0.06761, 30.68, 2, 0.06763, 67.628],
+                [48, 3, 1, 240, 0.24, 92.0267, 6, 0.20289, 202.884],
+                [0.23659, 0.14787, 0.00492, 1, 0.001, 0.45359, 0.02835, 0.001, 1],
+                [128, 17.07, 4.92892, 1000, 1, 453.592, 28.3495, 1, 1000],
+                [0.44, 0.03125, 0.01087, 2.20462, 0.0022, 1, 0.0625, 0.00221, 2.2],
+                [4.5, 0.5, 0.16667, 35.274, 0.03527, 16, 1, 0.03381, 33.814],
+                [236.588, 50, 4.92892, 1000, 1, 453.592, 29.5735, 1, 1000],
+                [0.236588, 0.01479, 0.00493, 1, 0.001, 0.45359, 0.02957, 0.001, 1],
+            ],
         };
     }
 
@@ -30,11 +45,12 @@ class Calculator extends React.Component {
                 ...prevState.listItems,
                 <IngredientListItem
                     calculatedQuantity={0}
-                    changeHandler={this.handleQuantityChange}
                     currentQuantity={0}
                     deleteHandler={this.removeListItemHandler}
                     id={prevState.listItemKeyCounter + 1}
                     key={prevState.listItemKeyCounter + 1}
+                    unitChangeHandler={this.handleUnitChange}
+                    quantityChangeHandler={this.handleQuantityChange}
                 />,
             ],
             listItemKeyCounter: prevState.listItemKeyCounter + 1,
@@ -53,10 +69,24 @@ class Calculator extends React.Component {
         }));
     };
 
+    handleUnitChange = (listItemIndex, unitPosition, unitValue) => {
+        this.setState((prevState) => ({
+            listItems: prevState.listItems.map((element) => {
+                switch (unitPosition) {
+                case 1:
+                    return element.props.id == listItemIndex ? {...element, props: {...element.props, fromUnit: unitValue}} : element;
+                case 2:
+                    return element.props.id == listItemIndex ? {...element, props: {...element.props, toUnit: unitValue}} : element;
+                }
+            }),
+        }));
+    };
+
     calculateQuantities = () => {
         const newList = [];
         this.state.listItems.forEach((element, index) => {
-            const calculatedQuantity = element.props.currentQuantity * (this.state.desiredServings / this.state.currentServings);
+            const ratio = this.state.conversionRatios[element.props.toUnit][element.props.fromUnit];
+            const calculatedQuantity = (element.props.currentQuantity * ratio) * (this.state.desiredServings / this.state.currentServings);
             newList[index] = {
                 ...element,
                 props: {...element.props, calculatedQuantity: calculatedQuantity}};
--- src/components/IngredientListItem/IngredientListItem.js
+++ src/components/IngredientListItem/IngredientListItem.js
@@ -1,18 +1,36 @@
-import {Stack, ListItem, TextField, Button} from '@mui/material';
+import {Stack, ListItem, TextField, Button, Select, MenuItem} from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import React from 'react';
 import PropTypes from 'prop-types';
 
 class IngredientListItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            // Supported measurement units (specifically in this order)
+            units: ['cup', 'tbsp', 'tsp', 'kg', 'g', 'lb', 'oz', 'mL', 'L'],
+        };
+    }
+
     render() {
         return <>
             <ListItem disableGutters>
                 <Stack direction="row" spacing={2}>
                     <TextField label="Item Name" variant="outlined" inputProps={{autoComplete: 'off'}} fullWidth />
                     <TextField label="Quantity" defaultValue={0} variant="outlined" autoComplete="off" type={'number'}
-                        inputProps={{autoComplete: 'off', min: 0}} onChange={(event) => this.props.changeHandler(this.props.id, (parseFloat(event.target.value)) || 0)} />
+                        inputProps={{autoComplete: 'off', min: 0}} onChange={(event) => this.props.quantityChangeHandler(this.props.id, (parseFloat(event.target.value)) || 0)} />
+                    <Select defaultValue={0} autoWidth onChange={(event) => this.props.unitChangeHandler(this.props.id, 1, (parseInt(event.target.value)) || 0)}>
+                        {this.state.units.map((unit, index) => (
+                            <MenuItem key={index} value={index}>{unit}</MenuItem>
+                        ))}
+                    </Select>
                     <TextField label="Calculation" variant="outlined" value={this.props.calculatedQuantity}
                         inputProps={{readOnly: true, disabled: true}} />
+                    <Select defaultValue={0} autoWidth onChange={(event) => this.props.unitChangeHandler(this.props.id, 2, (parseInt(event.target.value)) || 0)}>
+                        {this.state.units.map((unit, index) => (
+                            <MenuItem key={index} value={index}>{unit}</MenuItem>
+                        ))}
+                    </Select>
                     <Button onClick={() => this.props.deleteHandler(this.props.id)}>
                         <DeleteIcon color="action" fontSize="large"/>
                     </Button>
@@ -24,9 +42,10 @@ class IngredientListItem extends React.Component {
 
 IngredientListItem.propTypes = {
     calculatedQuantity: PropTypes.number.isRequired,
-    changeHandler: PropTypes.func.isRequired,
     deleteHandler: PropTypes.func.isRequired,
     id: PropTypes.number.isRequired,
+    unitChangeHandler: PropTypes.func.isRequired,
+    quantityChangeHandler: PropTypes.func.isRequired,
 };
 
 export default IngredientListItem;
